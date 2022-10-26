b522cf3e1efc4b443922e27b9bffb4ecbcc49246
Alparslan <maslan11alp@gmail.com>
2022-10-26 09:15:47 +0300

    cleanSheet26102022

--- src/test/java/tests/alparslan/US011.java
+++ src/test/java/tests/alparslan/US011.java
@@ -11,34 +11,30 @@ import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.TestBaseReport;
 
-
 import java.time.Duration;
 import java.util.Random;
 
-public class US011_TC002 extends TestBaseReport {
-    //https://allovercommerce.com/ Url'e gidilir
-    //Sign In butonuna tiklanir
-    //kullanici adi ve sifre girilir
-    //Sıgn In butonuna tiklanir
-    //My Account butonuna tiklanir
-    //Store Manager tiklanir
-    //Products tiklanir
-    //Belirtilen urune tiklanir
-    //Toptan Urun Gosterme sekmesine tiklanir
-    //Piece Type'i Kg secin
-    //MUnits Per Piece'e fiyat giriniz
-    //Min Order Quantity?'e miktar giriniz
-    //Submit tiklanir
-    //Product Successfully Published. yazisinin goruntulendigini dogrulayin
+public class US011 extends TestBaseReport {
+
+
     AlparslanPage alparslanPage;
     WebDriverWait wait;
     Random rnd;
-    Select select;
     JavascriptExecutor jse;
+    Select select;
 
     @Test
-    public void toptanUrunGostermeUs011TestCase002Test() throws InterruptedException {
-        extentTest = extentReports.createTest("Pozitif Test", "Satista olan random bir urunun Piece Type, Units Per Piece ve Min Order Quantity ozelliklerinin etkinligi test edildi ");
+    public void testEntry11() throws InterruptedException {
+        //https://allovercommerce.com/ Url'e gidilir
+        //Sign In butonuna tiklanir
+        //kullanici adi ve sifre girilir
+        //Sıgn In butonuna tiklanir
+        //My Account butonuna tiklanir
+        //Store Manager tiklanir
+        //Products tiklanir
+        //random bir urune tiklanir
+
+        extentTest = extentReports.createTest("Pozitif Test", "Urun Gosterme ozellikleri");
         //https://allovercommerce.com/ Url'e gidilir
         Driver.getDriver().get(ConfigReader.getProperty("url"));
         extentTest.info("allovercommerce sitesine gidildi");
@@ -46,17 +42,17 @@ public class US011_TC002 extends TestBaseReport {
         //Sign In butonuna tiklanir
         alparslanPage = new AlparslanPage();
         alparslanPage.homePageSignInButton.click();
-        extentTest.info("Sıgn In butonuna tiklandi.");
+        extentTest.info("sign in butonuna basıldı");
 
         //kullanici adi ve sifre girilir
         alparslanPage.signInPopUpUsernameTextBox.sendKeys("fikeka2419@charav.com");
         alparslanPage.signInPopUpPasswordTextBox.sendKeys("asd123456");
-        extentTest.info("Gecerli kullanici adi ve sifre girildi");
+        extentTest.info("Doğru kullanıcı email ve password girildi");
 
         //Sıgn In butonuna tiklanir
         alparslanPage.signInPopUpSignInButton.click();
         Thread.sleep(2000);
-        extentTest.info("Sıgn In butonuna tiklandi");
+        extentTest.info("PopUp uzerindeki Ikinci sign in butonuna basıldı");
 
         //My Account butonuna tiklanir
         jse = (JavascriptExecutor) Driver.getDriver();
@@ -74,16 +70,48 @@ public class US011_TC002 extends TestBaseReport {
         jse.executeScript("arguments[0].click();", alparslanPage.storeManagerPageProductsButton);
         extentTest.info("Products butonuna tiklandi");
 
-        //Belirtilen urune tiklanir
+        //random bir urune tiklanir
         rnd = new Random();
         int randomSayi = rnd.nextInt(alparslanPage.productsPageproductsList.size());
-        jse.executeScript("arguments[0].scrollIntoView(true);", alparslanPage.productsPageproductsList.get(randomSayi));
         jse.executeScript("arguments[0].click();", alparslanPage.productsPageproductsList.get(randomSayi));
         extentTest.info("Random bir urun tiklandi");
 
         //Toptan Urun Gosterme sekmesine tiklanir
         jse.executeScript("arguments[0].click();", alparslanPage.productsPageTopUrunGostermeAyarlar);
+        Thread.sleep(1000);
         extentTest.info("Toptan Urun Gosterme butonuna tiklandi");
+    }
+
+    @Test(dependsOnMethods = "testEntry11", priority = 1)
+    public void topUrunGosterme_US011_TC001_Test() throws InterruptedException {
+
+        //Toptan Urun Gosterme sekmesine tiklanir
+        //Piece Type goruntulendigini test edin
+        //MUnits Per Piece goruntulendigini test edin
+        //Min Order Quantity? goruntulendigini test edin
+
+        //Piece Type goruntulendigini test edin
+        Assert.assertTrue(alparslanPage.topUrunGayarlarpieceType.isEnabled(), "ToptanUrunGostermeAyarlarinda PieceType dogrulanamadi");
+        extentTest.info("Piece Type'in goruntulendigi test edildi");
+
+        //Units Per Piece goruntulendigini test edin
+        Assert.assertTrue(alparslanPage.topUrunGayarlarUnitPerPiece.isEnabled(), "ToptanUrunGostermeAyarlarinda Units Per Piece dogrulanamadi");
+        extentTest.info("Units Per Piece'in goruntulendigi test edildi");
+
+        //Min Order Quantity? goruntulendigini test edin
+        Assert.assertTrue(alparslanPage.topUrunGayarlarMinOrderQuantity.isDisplayed(),
+                "ToptanUrunGostermeAyarlarinda Min Order Quantity dogrulanamadi");
+        extentTest.pass("Min Order Quantity'nin goruntulendigi test edildi ");
+    }
+
+    @Test(dependsOnMethods = "testEntry11", priority = 2)
+    public void topUrunGosterme_US011_TC002_Test() throws InterruptedException {
+
+        //Piece Type'i Kg secin
+        //MUnits Per Piece'e fiyat giriniz
+        //Min Order Quantity?'e miktar giriniz
+        //Submit tiklanir
+        //Product Successfully Published. yazisinin goruntulendigini dogrulayin
 
         //Piece Type'i Kg secin
         select = new Select(alparslanPage.topUrunGayarlarpieceType);
@@ -117,8 +145,5 @@ public class US011_TC002 extends TestBaseReport {
         Assert.assertEquals(alparslanPage.topUrunGayarlarProductSuccesfullyPublishedText.getText(), expectedText);
         //Assert.assertTrue(alparslanPage.topUrunGayarlarProductSuccesfullyPublishedText.isDisplayed());
         extentTest.pass("Product Successfully Published. yazisinin goruntulendigi test edildi.");
-
-
-
     }
 }
